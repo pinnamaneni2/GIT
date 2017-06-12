39fe58d678c7ec19344941dc695c30b887694017
satishkumar <satishkumar@pinnamaneni>
2017-06-12 12:56:41 -0500

    intial check in

---
+++ src/satish/com/GitUse.java
@@ -0,0 +1,10 @@
+package satish.com;
+
+public class GitUse {
+	public static void main(String[] args) {
+		System.out.println("welcome to oxygen");
+		
+		HowToUseGit HTUG = new HowToUseGit(); 
+		HTUG.Thisway();	
+	}
+}
---
+++ src/satish/com/HowToUseGit.java
@@ -0,0 +1,7 @@
+package satish.com;
+
+public class HowToUseGit {
+void Thisway() {
+	System.out.println("THis way we can use git");
+}
+}
